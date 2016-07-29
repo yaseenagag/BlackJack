ad1a9019c58faf3fb31b7bf43d45ee0acd3e3161
Harman Singh <harmansingh@me.com>
2016-07-29 12:47:50 -0700

    Final updates (#11)
    * Fixed bug with dealer and player if statements
    
    * Fixed bugs with dealer if statements
    
    * Fixed error of displaying too many statements

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ deck.shuffle()
 const player = new Player()
 const dealer = new Player()
 
+//Game Intro
+console.log( "Welcome to Yaseen, Jrob & Harman's BlackJack" )
+console.log( "\nYour goal is to get a score of 21" )
+console.log( "\nIf you go over 21, you automatically lose" )
+//Card Values Explanation
+console.log( "\nCard Values:" )
+console.log( "\nCards 2-9 are Face Value" )
+console.log( "\nJack, Queen, King are worth 10 Points each" )
+console.log( "\nAce can be either 11 or 1 depending on current score" )
+console.log( "\nType 'H' to Hit (Get more cards) or 'S' to Stay\n" )
+
+
 // Dealer gives out:
 // - one card face down to player
 player.giveCard( deck.playCard() )
@@ -29,14 +41,20 @@ dealer.giveCard( deck.playCard() )
 // Player chooses to hit or stay
 // - If hits and bust (go over 21), automatically loses
 
-//PLAYER CODE
+//PLAYER variables
 let answer = 'H'
 let playerTotal = player.handValue()
 
 console.log( 'Player Hand: ', player.showHand() )
 
-if( playerTotal === 21 ) {
-  console.log( 'PLAYER GETS BLACKJACK!' )
+if ( playerTotal === 21 ) {
+  answer = 'S'
+}
+
+//DEALER Variable
+let dealerTotal = dealer.handValue()
+
+if ( dealerTotal === 21 ) {
   answer = 'S'
 }
 
@@ -48,6 +66,7 @@ while( answer !== 'S'  ) {
       player.giveCard( deck.playCard() )
       playerTotal = player.handValue()
       console.log( 'Player Hand: ', player.showHand() )
+      console.log( 'Score: ', playerTotal )
     break
     case 'S':
       break
@@ -60,32 +79,39 @@ while( answer !== 'S'  ) {
   }
 }
 
-//DEALER CODE
-let dealerTotal = dealer.handValue()
+if ( answer === 'S') {
+  console.log( "PLAYER CHOOSES TO STAY" )
+}
 // Dealer plays, hitting while 16 or under (17 or more, dealer stays)
 // - If hits and bust, player wins
 while( dealerTotal <= 16 && playerTotal < 21 ) {
   dealer.giveCard( deck.playCard() )
 
   dealerTotal = dealer.handValue()
-  console.log( 'Dealer Hand: ', dealer.showHand() )
+  console.log( '\nDealer Hand: ', dealer.showHand() )
 }
 
 //Show Scores and Hands for Player and Dealer
 console.log( '\nPLayer: ', player.showHand() )
-console.log( 'Score: ', playerTotal )
+console.log( 'Final Score: ', playerTotal )
 console.log( '\nDealer: ', dealer.showHand() )
-console.log( 'Score: ', dealerTotal )
+console.log( 'Final Score: ', dealerTotal )
 
 //if statements for player and dealer that do not change values
-if ( dealerTotal === 21 ) { //&& playerTotal !== 21
-  console.log( '\nYOU THE BESTEREST DEALER. DEALER WINS BECAUSE OF NATURAL' )
-}
-if ( playerTotal > 21 ) {
-  console.log( '\nPLAYER WENT OVER 21\nDEALER WINS\nPLAYER NEEDS PRACTICE' )
+// if ( dealerTotal === 21 ) { //&& playerTotal !== 21
+//   console.log( '\nYOU THE BESTEREST DEALER. DEALER WINS BECAUSE OF NATURAL' )
+// }
+if ( playerTotal === 21 ) {
+ console.log( '\nPLAYER GETS NATURAL BLACKJACK!\n' )
+} else if ( dealerTotal === 21 ) { //&& playerTotal !== 21
+  console.log( '\nYOU THE BESTEREST DEALER. DEALER WINS BECAUSE OF NATURAL BLACKJACK\n' )
+} else if ( playerTotal > 21 ) {
+  console.log( '\nPLAYER WENT OVER 21\nDEALER WINS\nPLAYER NEEDS PRACTICE\n' )
 } else if ( dealerTotal > 21) {
-  console.log( '\nYOU ROCK! YOU SHOULD HIT UP VEGAS\nDEALER WENT OVER 21' )
+  console.log( '\nYOU ROCK! YOU SHOULD HIT UP VEGAS\nDEALER WENT OVER 21\n' )
 // Compare player total to dealer total, highest wins
 } else if ( dealerTotal > playerTotal ) {
   console.log( '\nLIFE IS ABOUT TAKING RISKS! YOU SHOULD HAVE HIT AGAIN!\nDEALER WINS\n')
+} else if ( playerTotal > dealerTotal ) {
+  console.log( '\nGOOD JOB! YOU SCORED HIGHER THAN DEALER!\nDEALER LOSES!\n')
 }
